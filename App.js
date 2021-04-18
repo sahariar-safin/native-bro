1d1c76b291ca8e1c77a8e3c0e4898d9dfb7a2c64
Shafin The Developer <sahariar350@gmail.com>
2021-04-19 00:10:43 +0600

    something intersting

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>It is my Native Bro!</Text>
+      <Text>And I am useing React Native for mobile application first time!</Text>
+      <Text>Hi, {name}</Text>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <TextInput
+        style={{
+          height: 40,
+          width: 240,
+          fontSize: 23,
+          paddingLeft: 5,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue={name}
+        onChangeText={text => setName(text)}
+      />
       <StatusBar style="auto" />
     </View>
   );
